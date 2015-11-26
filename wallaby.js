778f390550aed3096e29014f59f25451823f2ac7
Artem Govorov <artem.govorov@gmail.com>
2015-11-26 12:54:18 +1000

    Babel 6 note

--- wallaby.js
+++ wallaby.js
@@ -15,7 +15,14 @@ module.exports = function (wallaby) {
     compilers: {
       '**/*.js': wallaby.compilers.babel({
         babel: babel,
-        // https://babeljs.io/docs/usage/experimental/
+        
+        // NOTE: If you're using Babel 6, it should be `presets: ['es2015']` instead of `stage: 0`.
+        // You will also need to
+        // npm install babel-core (and require it instead of babel)
+        // and
+        // npm install babel-preset-es2015
+        // See http://babeljs.io/docs/plugins/preset-es2015/
+
         stage: 0
       })
     }
